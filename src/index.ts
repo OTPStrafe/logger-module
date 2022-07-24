8fd1c47f114ceea80e394fb388f586faa9e6454c
OTPStrafe <otpstrafe@gmail.com>
2022-07-24 14:40:02 -0300

    dir fix

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import colors from 'colors';
 export const logger =  async (message: string, type: 'error' | 'info' | 'warn', logName?: string, dirName?: string, dirPath?: string): Promise<any> => {
   var name: string = logName || '[LOGGER]'; // Nombre del log
   var filePath: string; // Path del archivo/carpeta
-  if (!dirPath) filePath = path.join(__dirname, `../${dirName || 'logs'}`)
+  if (!dirPath) filePath = path.join(__dirname, `../../../${dirName || 'logs'}`)
   else filePath = dirPath;
   if (!filePath) return console.log(colors.red('No file path provided'));
   (type === 'error') ? console.log(colors.red(`${name}: ${message} - ${type}`.toUpperCase())) : undefined;
@@ -20,3 +20,5 @@ export const logger =  async (message: string, type: 'error' | 'info' | 'warn', 
     fs.appendFileSync(`${filePath}/log.txt`, `\n${name}: ${message} - ${type}`, { encoding: 'utf8' });
   }
 }
+
+logger('MENSAJE', 'info', '[RAGEMP]', 'information', 'D:\\Programacion\\workspace\\npm-test\\logs')
